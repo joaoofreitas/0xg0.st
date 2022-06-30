0e37794c3695404d75d60dca4da699fa5e17a0a9
joaoofreitas <joaoofreitas@pm.me>
2022-06-30 20:27:34 +0100

    Added keys by folder

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/golang/glog"
 )
@@ -14,6 +16,10 @@ import (
 func upload(w http.ResponseWriter, r *http.Request) {
 	glog.Info("Request recieved")
 
+	var uuid = GenerateUUID()
+	path := fmt.Sprintf("./storage/%s/", uuid)
+
+	// Prepare to get the file
 	file, header, err := r.FormFile("file")
 	if err != nil {
 		glog.Errorf("Error retrieving file. Error: %s", err.Error())
@@ -24,36 +30,52 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		glog.Infof(`File "%s" closed.`, header.Filename)
 	}()
 
+	// Creates directory with UUID
+	_, err = os.Stat(path)
+	for !os.IsNotExist(err) {
+		uuid = GenerateUUID()
+		path := fmt.Sprintf("./storage/%s/", uuid)
+		_, err = os.Stat(path)
+	}
+
+	if err := os.Mkdir(path, 0777); err != nil {
+		glog.Error("Error saving file on server...")
+	}
+
+	// Build and Write the file.
 	bytes, err := ioutil.ReadAll(file)
 	if err != nil {
 		glog.Errorf("Error retrieving file content.\n Error: %s", err.Error())
 	}
-
-	err = os.WriteFile(fmt.Sprintf("./storage/%s", header.Filename), bytes, 0644)
+	err = os.WriteFile(path+header.Filename, bytes, 0777)
 	if err != nil {
 		glog.Errorf("Error writing file.\n Error: %s", err.Error())
 	}
 
-	//// Building the file and reading all bytes from array
-	//tempFile, err := ioutil.TempDir("storage", header.Filename)
-	//bytes, err := ioutil.ReadAll(file)
-	//if err != nil {
-	//	glog.Errorf("Error retrieving file. Error: %s", err.Error())
-	//}
-	//defer tempFile.Close()
-	//tempFile.Write(bytes)
-
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
-
+	// To review all possible status codes
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "Successfully Uploaded File\n http://localhost:8000/%s\n", uuid)
 }
 
-// Get Alder-32 Hash.
-func hash(w http.ResponseWriter, r *http.Request) {
+// Generate a UUID
+func GenerateUUID() string {
+	var symbols = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890")
+	var uuid string
+	for i := 0; i < 12; i++ {
+		uuid += string(symbols[rand.Intn(len(symbols)-1)])
+	}
+
+	return uuid
+}
+
+// Get Adler-32 Hash.
+func getFile(w http.ResponseWriter, r *http.Request) {
 
 }
 
 // Route handling and application serving
 func main() {
+	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
 	flag.Usage = func() { fmt.Println("USAGE: To implement") }
 	flag.Set("logtostderr", "true")
 	flag.Set("stderrthreshold", "WARNING")
@@ -63,6 +85,6 @@ func main() {
 
 	glog.Flush()
 	http.HandleFunc("/", upload)
-	http.HandleFunc("/:hash", hash)
+	http.HandleFunc("/:hash", getFile)
 	http.ListenAndServe(":8000", nil)
 }

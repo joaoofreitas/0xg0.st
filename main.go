29c0d742a38111332fbc30be5cc0fa4df49bf9b9
joaoofreitas <joaoofreitas@pm.me>
2022-07-01 03:35:51 +0100

    Added logging and extra status codes

--- main.go
+++ main.go
@@ -34,8 +34,11 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	// Prepare to get the file
 	file, header, err := r.FormFile("file")
 	if err != nil {
-		glog.Errorf("Error retrieving file. Error: %s", err.Error())
-		return
+		glog.Errorf("Error retrieving file.")
+		glog.Errorf("Error: %s", err.Error())
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "Bad request. Error retrieving file.")
+
 	}
 	defer func() {
 		file.Close()
@@ -52,27 +55,38 @@ func upload(w http.ResponseWriter, r *http.Request) {
 
 	if err := os.Mkdir(path, 0777); err != nil {
 		glog.Error("Error saving file on server...")
+		glog.Errorf("Error: %s", err.Error())
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "No storage available.")
 	}
 
 	// Build and Write the file.
 	bytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		glog.Errorf("Error retrieving file content.\n Error: %s", err.Error())
+		glog.Errorf("Content not readable.")
+		glog.Errorf("Error: %s", err.Error())
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Internal Server Error. Content not readable.")
 	}
 	err = os.WriteFile(path+header.Filename, bytes, 0777)
 	if err != nil {
-		glog.Errorf("Error writing file.\n Error: %s", err.Error())
+		glog.Errorf("Error writing file.")
+		glog.Errorf("Error: %s", err.Error())
+		w.WriteHeader(507)
+		fmt.Fprintf(w, "Insufficient Storage. Error storing file.")
 	}
 
-	// To review all possible status codes
+	// All good
 	w.WriteHeader(200)
-	fmt.Fprintf(w, "Successfully Uploaded File\n http://localhost:8000/id/%s\n", uuid)
+	fmt.Fprintf(w, "OK, Successfully Uploaded\n http://localhost:8000/id/%s\n", uuid)
 }
 
+// Gets the file by the ID provided
 func getFile(w http.ResponseWriter, r *http.Request) {
 	//We will get under path and storage the only file that will be inside and return it to the client
-	var re = regexp.MustCompile(`(?m)[^\/]+$`)
 	var uuid string
+
+	var re = regexp.MustCompile(`(?m)[^\/]+$`)
 	for _, match := range re.FindAllString(r.URL.Path, -1) {
 		uuid = match
 	}
@@ -85,13 +99,22 @@ func getFile(w http.ResponseWriter, r *http.Request) {
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		glog.Errorf(`Error walking filepath "%s"`, path)
+		glog.Errorf("Error: %s", err.Error())
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "File Not Found.")
 	}
 
-	var file = files[0]
-	glog.Infof(`Retrieving Filename "%s"`, "./"+file.Name())
-	w.Header().Set("Content-Disposition", "attachment; filename="+file.Name())
-	http.ServeFile(w, r, "./"+path+"/"+file.Name())
+	if len(files) <= 0 {
+		glog.Errorf(`No files in directory "%s"`, path)
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "File Not Found.")
+	}
 
+	var filename = files[0].Name()
+	glog.Infof(`Retrieving Filename "%s"`, fmt.Sprintf("./%s", filename))
+
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", filename))
+	http.ServeFile(w, r, fmt.Sprintf("./%s/%s", path, filename))
 }
 
 // Route handling, logging and application serving
